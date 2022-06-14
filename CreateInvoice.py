0ae98439b7877e63fcbb93bdaebd868a50c99a4c
Jarawr <jarraramjad@gmail.com>
2022-06-14 14:16:53 +0500

    Code almsot complete

--- CreateInvoice.py
+++ CreateInvoice.py
@@ -2,15 +2,29 @@ from openpyxl import Workbook, load_workbook
 import json
 
 EXCEL_FILE = "Notebook-Excel.xlsx"
+invoice_workbook = load_workbook(EXCEL_FILE)
+invoice_worksheet = invoice_workbook.active
 
 with open('Customer Info.json', 'r') as fp:
   customer_info = json.load(fp)
 
-# invoice_workbook = load_workbook(EXCEL_FILE)
-# invoice_worksheet = invoice_workbook.active
 
-# invoice_worksheet['D6'].value = "Test Address"
+for customer, details in customer_info.items():
 
-# invoice_workbook.save(EXCEL_FILE)
+    invoice_worksheet['I6'].value = customer
+
+    Ship_to = details['Ship_to']
+    invoice_worksheet['D6'].value= Ship_to
+
+    Quantity = details['Quantity']
+    invoice_worksheet['G15'].value= int(Quantity)
+    
+    Ship_date = details['Ship_date']
+    invoice_worksheet['I7'].value= Ship_date
+
+    Unit_price = details['Unit_price']
+    invoice_worksheet['H15'].value= float(Unit_price.replace('£',''))
+
+    invoice_workbook.save('Invoices\\'+'Invoice-'+customer+'.xlsx')
 
 
