6c24cd3ce6e7cd6a499dfde6ff8351f933a68385
Jarawr <jarraramjad@gmail.com>
2022-06-16 18:56:31 +0500

    Document number text box not working, successfully uploaded documents, Refresh not working

--- CreateInvoice.py
+++ CreateInvoice.py
@@ -4,8 +4,10 @@ import os
 
 
 def excel_to_pdf(xlsx_invoice_path):
+
   try:
     xl_file = win32com.client.Dispatch("Excel.Application")
+
     xl_file.Visible = False
 
     wb = xl_file.Workbooks.Open(xlsx_invoice_path)
@@ -38,41 +40,44 @@ def excel_to_pdf(xlsx_invoice_path):
     print('error occoured trying to convert to pdf: ', e)
     wb.Close(True)
 
-# with open('Customer Info.json', 'r') as fp:
-#   customer_info = json.load(fp)
 
 def create_invoice(Order_id, customer_info):
-  
-  TEMPLATE_FILE = "Base-Template.xlsx"
-  # Open template invoice
-  invoice_workbook = load_workbook(TEMPLATE_FILE)
-  invoice_worksheet = invoice_workbook.active
-  
-  # Change data in template invoice according to what you got from customer_info
-  # Invoice number / Order id
-  invoice_worksheet['I6'].value = Order_id
-
-  Ship_to = customer_info[Order_id]['Ship_to']
-  invoice_worksheet['D6'].value= Ship_to
-
-  Quantity = customer_info[Order_id]['Quantity']
-  invoice_worksheet['G15'].value= int(Quantity)
-  
-  Ship_date = customer_info[Order_id]['Ship_date']
-  invoice_worksheet['I7'].value= Ship_date
-
-  Unit_price = customer_info[Order_id]['Unit_price']
-  invoice_worksheet['H15'].value= float(Unit_price.replace('£',''))
 
   xlsx_invoice_path = os.getcwd()+'\\'+'Invoices\\'+Order_id+'.xlsx'
+  
+  # if excel file already exists, then return corresponding pdf
+  if os.path.exists(xlsx_invoice_path):
+    return xlsx_invoice_path.replace('.xlsx', '.pdf')
+  # else generate a new excel file / pdf
+  else:
+    TEMPLATE_FILE = "Base-Template.xlsx"
+    # Open template invoice
+    invoice_workbook = load_workbook(TEMPLATE_FILE)
+    invoice_worksheet = invoice_workbook.active
+    
+    # Change data in template invoice according to what you got from customer_info
+    # Invoice number / Order id
+    invoice_worksheet['I6'].value = Order_id
 
-  # Save the modified invoice as an excel file
-  invoice_workbook.save(xlsx_invoice_path)
-  # close the workbook
-  invoice_workbook.close()
-  # convert the excel file into a pdf file, pass in the path of the excel file
-  pdf_invoice_path= excel_to_pdf(xlsx_invoice_path)
-  # return the path of the converted pdf file
-  return pdf_invoice_path
+    Ship_to = customer_info[Order_id]['Ship_to']
+    invoice_worksheet['D6'].value= Ship_to
+
+    Quantity = customer_info[Order_id]['Quantity']
+    invoice_worksheet['G15'].value= int(Quantity)
+    
+    Ship_date = customer_info[Order_id]['Ship_date']
+    invoice_worksheet['I7'].value= Ship_date
+
+    Unit_price = customer_info[Order_id]['Unit_price']
+    invoice_worksheet['H15'].value= float(Unit_price.replace('£',''))
+
+    # Save the modified invoice as an excel file
+    invoice_workbook.save(xlsx_invoice_path)
+    # close the workbook
+    invoice_workbook.close()
+    # convert the excel file into a pdf file, pass in the path of the excel file
+    pdf_invoice_path= excel_to_pdf(xlsx_invoice_path)
+    # return the path of the converted pdf file
+    return pdf_invoice_path
 
 
--- Fetch_Customer_Details.py
+++ Fetch_Customer_Details.py
@@ -13,17 +13,18 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import os
+import time
 import json
 from collections import OrderedDict
 from CreateInvoice import create_invoice
+from selenium.common.exceptions import NoSuchElementException        
 
-
-def writeToJSONFile(data):
-    with open('Customer Info.json', 'w') as fp:
-        json.dump(data, fp)
-
-
+def check_exists_by_xpath(chromedriver, xpath):
+    try:
+        chromedriver.find_element(By.XPATH, xpath)
+    except NoSuchElementException:
+        return False
+    return True
 
 opt= Options()
 opt.add_experimental_option("debuggerAddress", "localhost:8989")
@@ -37,10 +38,10 @@ driver.switch_to.window(driver.current_window_handle)
 chwd = driver.window_handles
 
 customer_info = OrderedDict()
-
+i = 0
 # loop through each window performm certain tasks
 for child_window in chwd:
-
+    i+=1
     # Benjamin Johns
     # 24
     # AUSTEN DRIVE
@@ -48,40 +49,74 @@ for child_window in chwd:
     # BS22 7UY
     # United Kingdom
     Ship_to = driver.find_element(By.XPATH, "//*[@id='MYO-app']/div/div[1]/div[1]/div/div[2]/div[2]/div/div/div/div/div[1]/table/tbody/tr[1]/td/span/span[3]/div/div")
+    Ship_to = Ship_to.text
     # print(Ship_to.text)
 
     # 026-0968862-2197164
     Order_id = driver.find_element(By.XPATH, "//*[@id='MYO-app']/div/div[1]/div[1]/div/div[1]/div[1]/div/span[5]")
+    Order_id = Order_id.text
     # print(Order_id.text)
 
     # 2
     Quantity = driver.find_element(By.XPATH, "//*[@id='MYO-app']/div/div[1]/div[1]/div/div[6]/div/table/tbody/tr/td[5]")
+    Quantity = Quantity.text
     # print(Quantity.text)
 
     # Wed, 1 Jun 2022
     Ship_date = driver.find_element(By.XPATH, "//*[@id='MYO-app']/div/div[1]/div[1]/div/div[2]/div[1]/div/div/div/div/div/div[1]/table/tbody/tr[1]/td[2]/span")
+    Ship_date = Ship_date.text
     # print(Ship_date.text)
 
     # £10.95
     Unit_price = driver.find_element(By.XPATH, "//*[@id='MYO-app']/div/div[1]/div[1]/div/div[6]/div/table/tbody/tr/td[6]/span")
+    Unit_price = Unit_price.text
     # print(Unit_price.text)
 
+    # Shipping Price
+    """Pending Start"""
+    # if driver.find_element(By.XPATH, "//*[@id='MYO-app']/div/div[1]/div[1]/div/div[6]/div/table/tbody/tr/td[6]/span"):
+    #     # take value
+    #     Ship_price = driver.find_element(By.XPATH, "//*[@id='MYO-app']/div/div[1]/div[1]/div/div[6]/div/table/tbody/tr/td[6]/span").text
+    # else:
+    #     Ship_price = '0' 
+    """Pending End"""
+
     # keep saving all order id's with relevant stats to a dict of order id's ---> dict = {'Order_id_1':{'a':1,'b':2,'c':3}, 'Order_id_2':{...}...}
-    customer_info[Order_id.text]= {'Ship_to':Ship_to.text, "Quantity": Quantity.text, "Ship_date": Ship_date.text, "Unit_price": Unit_price.text}
+    customer_info[Order_id]= {'Ship_to':Ship_to, "Quantity": Quantity, "Ship_date": Ship_date, "Unit_price": Unit_price}
 
     # Create and save invoice
-    pdf_invoice_path = create_invoice(Order_id.text, customer_info)
+    pdf_invoice_path = create_invoice(Order_id, customer_info)
 
-    # Click on Upload button and wait
-    # upload_button = driver.find_element(By.XPATH, "//*[@id='MYO-app']/div/div[1]/div[1]/div/div[1]/div[2]/div[2]/span[1]")
-    # upload_button.click()
-    
-    # Wait for browse button to appear, then send the generated invoice via sendkeys method
-    # browse_button = WebDriverWait(driver, 5).until(
-    #     EC.presence_of_element_located((By.NAME, "invoice-file"))
-    #     ).send_keys(pdf_invoice_path)
 
-    print('Path of upload pdf: ', pdf_invoice_path)
+    while not check_exists_by_xpath(driver, "/html/body/div[3]/div/div/div/div/div[2]/form/span[2]"):
+        # Steps: Refresh -> Upload -> Browse
+            # if document uploaded then loop breaks
+            # else keep repeating the loop
+        print(Order_id)
+        # refresh the page before uploading any data
+        # driver.refresh()
+        # wait for a few seconds to make sure page loads
+        # time.sleep(3)
+
+        # Wait for upload button to appear and click it
+        WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[2]/div/div/div[1]/div[1]/div/div[1]/div[2]/div[2]/span[1]/span'))
+            ).click()
+
+        # print('CLICKED THE BUTTON TO OPEN PROMPT')
+        
+        # Wait for browse button to appear, then send the generated invoice via sendkeys method
+        browse_button = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.NAME, "invoice-file"))
+            ).send_keys(pdf_invoice_path)
+
+        # # fill in the document number box
+        # document_number = driver.find_element(By.XPATH, '//*[@id="invoice_number_input"]').send_keys(Order_id)
+        
+        # # wait for upload to process and document number to appear
+        # time.sleep(1)
+
+    print('Invoice Number'+str(i)+' Generated: ', pdf_invoice_path)
 
     # break
     driver.switch_to.window(child_window)
